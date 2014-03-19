bc93d27ecabb3ed86efc2eaec94fd725528ca109
Emilie Meade <emilie.meade@osumc.edu>
2014-03-19 09:05:52 -0400

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EmeadeDemo::Application.config.secret_key_base = '092ba1b7600d44ae9f16513e4cc5177d514df7b0312946dd80016849c56769d2c6a4764b0f9cf26943300c0a665a37f52f8e9a0782bd0ac55be8102ec022bd87'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+EmeadeDemo::Application.config.session_store :cookie_store, key: '_emeade-demo_session'
