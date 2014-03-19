7a82ce9723a1bdc4e0322420904442af8042eff3
Emilie Meade <emilie.meade@osumc.edu>
2014-03-19 19:43:13 -0400

    parse feed and save to database list data

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,7 @@
 class StaticPagesController < ApplicationController
   def home
+    #TODO: schedule updates
+    #Jobs.update_from_feed('https://www.jobsatosu.com/all_jobs.atom')
+    @jobs = Jobs.all(:limit => 10, :order => "published_at desc")
   end
 end
--- app/models/jobs.rb
+++ app/models/jobs.rb
@@ -1,2 +1,19 @@
 class Jobs < ActiveRecord::Base
+
+  def self.update_from_feed(feed_url)
+    feed = Feedjira::Feed.fetch_and_parse feed_url
+    feed.entries.each do |entry|
+      unless exists? :guid => entry.id
+        create!(
+            :title         => entry.title,
+            :content      => entry.content,
+            :url          => entry.url,
+            :author       => entry.author,
+            :published_at => entry.published,
+            :guid         => entry.id
+        )
+      end
+    end
+  end
+
 end
