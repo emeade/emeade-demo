412d64e0c694e2aa3a68d3989ee55726f5f4ecb2
Emilie Meade <emilie.meade@osumc.edu>
2014-03-23 13:21:48 -0400

    add default sort to the controller

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,6 +1,7 @@
 class JobsController < ApplicationController
   def index
     @search = Jobs.search(params[:q])
+    @search.sorts = 'published_at desc' if @search.sorts.empty?
     @jobs = @search.result(distinct: true)
   end
   def show
