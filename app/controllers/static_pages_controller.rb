b338e12643e3f0f1e9fb04fd5722c5327213b2d8
Emilie Meade <emilie.meade@osumc.edu>
2014-03-23 13:22:27 -0400

    add about and contact

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,7 +3,15 @@ class StaticPagesController < ApplicationController
     @search = Jobs.search(params[:q])
     @jobs = @search.result(distinct: true)
   end
+
   def style
+
+  end
+
+  def about
+  end
+
+  def contact
 
   end
 end
